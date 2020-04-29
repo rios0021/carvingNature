f60f80dc157664f13d82d786c4c9415b9c5c164e
Eduardo Rios Corlay <rios0021@algonquinlive.com>
2020-04-29 01:10:40 -0400

    Separated js code into new file js/main.js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(window).on('load', function () {
+    setTimeout(removeLoader, 3000);
+});
+
+window.onscroll = function () { scrollFunction() };
+
+function removeLoader() {
+    $("#pre-loader").fadeOut( "slow" );
+
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        $('nav').addClass('scrolled');
+        $('#navLogo').removeClass('logo-default');
+        $('#navLogo').addClass('logo-small');
+    } else {
+        $('nav').removeClass('scrolled');
+        $('#navLogo').addClass('logo-default');
+        $('#navLogo').removeClass('logo-small');
+    }
+}
